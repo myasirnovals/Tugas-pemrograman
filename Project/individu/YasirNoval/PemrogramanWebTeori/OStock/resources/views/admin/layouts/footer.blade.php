8c88c396b5451c9233c623145eae4b9faa69bb67
myasirnoval <myasirnoval@gmail.com>
2024-05-28 04:20:40 +0700

    laravel project 4: configure admin login success

---
+++ Project/individu/YasirNoval/PemrogramanWebTeori/OStock/resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- ./wrapper -->
+<!-- jQuery -->
+<script src="{{ asset('assets/admin/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('assets/admin/js/adminlte.min.js') }}"></script>
+<!-- AdminLTE for demo purposes -->
+{{--<script src="{{ asset('assets/admin/js/demo.js') }}"></script>--}}
+</body>
+</html>
