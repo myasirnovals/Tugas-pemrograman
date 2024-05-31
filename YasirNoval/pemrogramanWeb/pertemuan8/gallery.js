ae65e7c3ec8db37674687ee7b98da7cd72ac9f1f
yasir noval <aibnu0371@gmail.com>
2024-05-31 09:10:53 +0700

    pertemuan 8

--- YasirNoval/pemrogramanWeb/pertemuan8/gallery.js
+++ YasirNoval/pemrogramanWeb/pertemuan8/gallery.js
@@ -8,7 +8,7 @@ function openbox(url)
 
     var content=document.getElementById('boxcontent');
     content.style.padding="0";
-    content.innerHTML="<img src=" + url + "/>";
+    content.innerHTML= `<img src=${url} alt=${url} width="650" height="400">`;
     box.style.display='block';
 }
 
--- YasirNoval/pemrogramanWeb/pertemuan8/lightbox.js
+++ YasirNoval/pemrogramanWeb/pertemuan8/lightbox.js
@@ -1,14 +1,14 @@
 function openbox(url)
 {
-    var box=document.getElementById('box');
+    let box=document.getElementById('box');
     document.getElementById('shadowing').style.display='block';
 
-    var title=document.getElementById('boxtitle');
+    let title=document.getElementById('boxtitle');
     title.innerHTML=url;
 
-    var content=document.getElementById('boxcontent');
+    let content=document.getElementById('boxcontent');
     content.style.padding="0";
-    content.innerHTML="<img src=" + url + "/>";
+    content.innerHTML= `<img src=${url} alt=${url} width="650" height="400">`;
     box.style.display='block';
 }
 
