780db71b75b2740981b0ad751f708f3c7b4fbfd5
myasirnoval <myasirnoval@gmail.com>
2024-06-14 08:43:13 +0700

    update halaman user

--- Project/bersama/PemrogramanWeb/AryaYasirReza/KasirTopUp/view/user/main_content.php
+++ Project/bersama/PemrogramanWeb/AryaYasirReza/KasirTopUp/view/user/main_content.php
@@ -1,101 +1,33 @@
 <div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1 style="font-size: 25px;margin: 0;padding: 20px;text-shadow: 2px 2px 3px;text-align: center;">Select
-                Your
-                Game</h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-3">
-            <div class="card" style="margin: 10px;">
-                <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title" style="font-size: 20px;">Genshin Impact</h4>
-                    <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
-                    <picture><img></picture>
-                    <button class="btn btn-primary" type="button" style="padding: 6px 25%;margin: 15px;">Select
-                    </button>
-                </div>
+    <?php if (!empty($dataGames)) { ?>
+        <div class="row">
+            <div class="col-md-12 col-sm-12">
+                <h1 style="font-size: 25px;margin: 0;padding: 20px;text-shadow: 2px 2px 3px;text-align: center;">
+                    Select Your Game</h1>
             </div>
         </div>
-        <div class="col-md-3">
-            <div class="card" style="margin: 10px;">
-                <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title" style="font-size: 20px;">Wuthering Waves</h4>
-                    <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
-                    <picture><img></picture>
-                    <button class="btn btn-primary" type="button" style="padding: 6px 25%;margin: 15px;">Select
-                    </button>
+        <div class="row">
+            <?php foreach ($dataGames as $data) { ?>
+                <div class="col-md-3">
+                    <div class="card" style="margin: 10px;">
+                        <div class="card-body" style="text-align: center;">
+                            <h4 class="card-title" style="font-size: 20px;"><?= $data['nameGame']; ?></h4>
+                            <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
+                            <picture><img src="../../assets/images/game/<?= $data['image']; ?>" alt="gambar game" class="img-thumbnail"></picture>
+                            <button class="btn btn-primary" type="button" style="padding: 6px 25%;margin: 15px;">Select
+                            </button>
+                        </div>
+                    </div>
                 </div>
+            <?php } ?>
+        </div>
+    <?php } else { ?>
+        <div class="row">
+            <div class="col-md-12 col-sm-12">
+                <h1 style="font-size: 25px;margin: 0;padding: 20px;text-shadow: 2px 2px 3px;text-align: center;">
+                    Maaf Game Belum ditambahkan oleh Admin
+                </h1>
             </div>
         </div>
-        <div class="col-md-3">
-            <div class="card" style="margin: 10px;">
-                <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title" style="font-size: 20px;">Tower of Fantasy</h4>
-                    <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
-                    <picture><img></picture>
-                    <button class="btn btn-primary" type="button" style="padding: 6px 25%;margin: 15px;">Select
-                    </button>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card" style="margin: 10px;">
-                <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title" style="font-size: 20px;">Valorant</h4>
-                    <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
-                    <picture><img></picture>
-                    <button class="btn btn-primary" type="button" style="padding: 6px 25%;margin: 15px;">Select
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row" style="margin: 15px;">
-        <div class="col-md-3">
-            <div class="card" style="margin: 10px;">
-                <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title" style="font-size: 20px;">Counter Strike</h4>
-                    <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
-                    <picture><img></picture>
-                    <button class="btn btn-primary" type="button" style="padding: 6px 25%;margin: 15px;">Select
-                    </button>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card" style="margin: 10px;">
-                <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title" style="font-size: 20px;">PUBG</h4>
-                    <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
-                    <picture><img></picture>
-                    <button class="btn btn-primary" type="button" style="padding: 6px 25%;margin: 15px;">Select
-                    </button>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card" style="margin: 10px;">
-                <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title" style="font-size: 20px;">Mobile Legends</h4>
-                    <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
-                    <picture><img></picture>
-                    <button class="btn btn-primary" type="button" style="padding: 6px 25%;margin: 15px;">Select
-                    </button>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card" style="margin: 10px;">
-                <div class="card-body" style="text-align: center;">
-                    <h4 class="card-title" style="font-size: 20px;">Blue Archive</h4>
-                    <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
-                    <picture><img></picture>
-                    <button class="btn btn-primary" type="button" style="padding: 6px 25%;margin: 15px;">Select
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php } ?>
 </div>
