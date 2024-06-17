36208a61383f1dfc2e45bd91cb762e603ecca3e3
myasirnoval <myasirnoval@gmail.com>
2024-06-18 02:46:07 +0700

    update cara menghapus product dan game

--- Project/bersama/PemrogramanWeb/AryaYasirReza/KasirTopUp/app/DeleteData.php
+++ Project/bersama/PemrogramanWeb/AryaYasirReza/KasirTopUp/app/DeleteData.php
@@ -5,8 +5,13 @@ function DeleteGame()
 {
     $id = $_GET['id'];
 
+    // data for database
     $file_game = "../database/data_game" . ".json";
     $file_product = "../database/data_product" . ".json";
+
+    // data for image
+    $file_image_game = "../assets/images/game/";
+    $file_image_product = "../assets/images/product/";
 
     if (file_exists("$file_game") && file_exists("$file_product")) {
         $data_game = file_get_contents("$file_game");
@@ -21,9 +26,15 @@ function DeleteGame()
                     if ($product["gameCode"] == $game["kodeGame"]) {
                         array_splice($array_product, $key_product, GetUserAmount($game["kodeGame"]));
                     }
+
+                    // remove all image from directory
+                    unlink("$file_image_product" . $product['image']);
                 }
                 $array_product = json_encode($array_product, JSON_PRETTY_PRINT);
                 file_put_contents($file_product, $array_product);
+
+                // remove image from directory
+                unlink("$file_image_game" . $game['image']);
 
                 array_splice($array_game, $key_game, 1);
             }
@@ -42,6 +53,7 @@ function DeleteGameProduct()
     $id = $_GET['id'];
 
     $file_name = "../database/data_product" . ".json";
+    $file_image = "../assets/images/product/";
 
     if (file_exists("$file_name")) {
         $current_data = file_get_contents("$file_name");
@@ -49,6 +61,7 @@ function DeleteGameProduct()
 
         foreach ($array_data as $key => $value) {
             if ($value["productId"] == $id) {
+                unlink("$file_image" . $value['image']);
                 array_splice($array_data, $key, 1);
             }
         }
