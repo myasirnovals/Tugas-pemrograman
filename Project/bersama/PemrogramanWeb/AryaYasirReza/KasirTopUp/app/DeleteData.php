f67f8099046a1e125e4ef1b2bdd7959064e11327
myasirnoval <myasirnoval@gmail.com>
2024-06-17 16:06:14 +0700

    update proses hapus dari parent

--- Project/bersama/PemrogramanWeb/AryaYasirReza/KasirTopUp/app/DeleteData.php
+++ Project/bersama/PemrogramanWeb/AryaYasirReza/KasirTopUp/app/DeleteData.php
@@ -3,19 +3,37 @@ function DeleteGame()
 {
     $id = $_GET['id'];
 
-    $file_name = "../database/data_game" . ".json";
+    $file_game = "../database/data_game" . ".json";
+    $file_product = "../database/data_product" . ".json";
 
-    if (file_exists("$file_name")) {
-        $current_data = file_get_contents("$file_name");
-        $array_data = json_decode($current_data, true);
+    if (file_exists("$file_game") && file_exists("$file_product")) {
+        $data_game = file_get_contents("$file_game");
+        $array_game = json_decode($data_game, true);
 
-        foreach ($array_data as $key => $value) {
-            if ($value["kodeGame"] == $id) {
-                array_splice($array_data, $key, 1);
+        foreach ($array_game as $key_game => $game) {
+            if ($game["kodeGame"] == $id) {
+                $data_product = file_get_contents("$file_product");
+                $array_product = json_decode($data_product, true);
+
+                foreach ($array_product as $key_product => $product) {
+                    if ($product["gameCode"] == $game["kodeGame"]) {
+                        /*
+                         * Perbaiki baris code ini supaya bisa menghapus data
+                         * produk secara otomatis (maksudnya ketika productnya ada
+                         * 2 maka harus terhapus kedua nya, jika hanya satu maka
+                         * satu saja yang terhapus)*/
+                        array_splice($array_product, $key_product, 2);
+                    }
+                }
+                $array_product = json_encode($array_product, JSON_PRETTY_PRINT);
+                file_put_contents($file_product, $array_product);
+
+                array_splice($array_game, $key_game, 1);
             }
         }
 
-        return json_encode($array_data, JSON_PRETTY_PRINT);
+
+        return json_encode($array_game, JSON_PRETTY_PRINT);
 
     } else {
         return "Data not exist <br>";
