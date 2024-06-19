abab1daa3ba3c9c7cb1518501971e33318ef7b76
myasirnoval <myasirnoval@gmail.com>
2024-06-19 14:02:00 +0700

    halaman admin selesai

--- Project/bersama/PemrogramanWeb/AryaYasirReza/KasirTopUp/app/UpdateData.php
+++ Project/bersama/PemrogramanWeb/AryaYasirReza/KasirTopUp/app/UpdateData.php
@@ -146,3 +146,77 @@ function UpdateMember() {
         return json_encode($dataError);
     }
 }
+
+function UpdateProfile()
+{
+    $id = $_POST["memberId"];
+    $password = $_POST["password"];
+    $old_password = $_POST["old_password"];
+
+    $name = htmlspecialchars($_POST["name"]);
+    $birthdate = htmlspecialchars($_POST["birthdate"]);
+    $bio = htmlspecialchars($_POST["bio"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $address = htmlspecialchars($_POST["address"]);
+    $old_image = htmlspecialchars($_POST["old_image"]);
+
+    $updated_at = date('l, d / M / Y  H:i:s');
+
+    $file_image = "../assets/images/member/";
+
+    // verify image
+    if ($_FILES['image']['error'] === 4) {
+        $image = $old_image;
+    } else {
+        if (($old_image != "") && ($old_image != "-")) {
+            unlink("$file_image" . $old_image);
+        }
+        $image = UploadsImageMember();
+    }
+
+    // verify password
+    if ($password != "") {
+        $password = password_hash($password, PASSWORD_BCRYPT);
+    } else {
+        $password = $old_password;
+    }
+
+    $file_name = "../database/data_member" . ".json";
+
+    if (file_exists("$file_name")) {
+        $current_data = file_get_contents("$file_name");
+        $array_data = json_decode($current_data, true);
+
+        foreach ($array_data as $key => $value) {
+            if ($value["memberId"] == $id) {
+                $array_data[$key]["name"] = $name;
+                $array_data[$key]["birthdate"] = $birthdate;
+                $array_data[$key]["bio"] = $bio;
+                $array_data[$key]["email"] = $email;
+                $array_data[$key]["password"] = $password;
+                $array_data[$key]["address"] = $address;
+                $array_data[$key]["image"] = $image;
+                $array_data[$key]["updated_at"] = $updated_at;
+            }
+        }
+
+        return json_encode($array_data, JSON_PRETTY_PRINT);
+
+    } else {
+        $dataError = array();
+        $dataError[] = array(
+            "memberId" => $id,
+            "name" => $name,
+            "birthdate" => $birthdate,
+            "bio" => $bio,
+            "email" => $email,
+            "password" => $password,
+            "address" => $address,
+            "image" => $image,
+            "updated_at" => $updated_at
+        );
+
+        echo "File not exist <br>";
+        return json_encode($dataError);
+    }
+}
--- Project/bersama/PemrogramanWeb/AryaYasirReza/KasirTopUp/view/admin/profile.php
+++ Project/bersama/PemrogramanWeb/AryaYasirReza/KasirTopUp/view/admin/profile.php
@@ -1,9 +1,10 @@
 <?php include "../layout/header.php"; ?>
 <?php include "nav_bar.php"; ?>
-    <div class="container profile profile-view" id="profile">
-        <form action="" method="post" enctype="multipart/form-data">
-            <div class="row profile-row">
-                <div class="col-md-4 relative">
+    <div class="container mt-5 mb-5">
+        <form action="../../routes/update_profile.php" method="post" enctype="multipart/form-data">
+            <div class="row">
+            <div class="row">
+                <div class="col-md-4 col-sm-12 relative">
                     <div class="avatar">
                         <div class="avatar-bg center">
                             <?php foreach ($dataMembers as $data) { ?>
@@ -20,32 +21,37 @@
                         </div>
                         <br>
                     </div>
-                    <input class="form-control form-control" type="file" name="avatar-file">
+                    <input class="form-control mt-5" type="file" name="image">
                 </div>
-                <div class="col-md-8">
+                <div class="col-md-8 col-sm-12">
                     <?php foreach ($dataMembers as $data) { ?>
                         <?php if ($_SESSION['email'] == $data['email']) { ?>
-                            <input type="hidden" name="old_password" id="old_password" value="<?= $data['password']?>">
-                            <input type="hidden" name="old_image" id="old_image" value="<?= $data['image']?>">
+                            <input type="hidden" name="old_password" id="old_password" value="<?= $data['password'] ?>">
+                            <input type="hidden" name="memberId" id="memberId" value="<?= $data['memberId'] ?>">
+                            <input type="hidden" name="old_image" id="old_image" value="<?= $data['image'] ?>">
+                            <input type="hidden" name="old_birthdate" id="old_birhtdate" value="<?= $data['birthdate'] ?>">
                             <h1>Profile </h1>
                             <hr>
                             <div class="row">
                                 <div class="col-sm-12 col-md-6">
                                     <div class="form-group mb-3">
                                         <label class="form-label" for="name">Nama </label>
-                                        <input class="form-control" type="text" name="name" id="name" value="<?= $data['name']?>">
+                                        <input class="form-control" type="text" name="name" id="name"
+                                               value="<?= $data['name'] ?>">
                                     </div>
                                 </div>
                                 <div class="col-sm-12 col-md-6">
                                     <div class="form-group mb-3">
                                         <label class="form-label" for="birthdate">Tanggal Lahir </label>
-                                        <input class="form-control" type="date" name="birthdate" id="birthdate" value="<?= $data['birthdate']?>">
+                                        <input class="form-control" type="date" name="birthdate" id="birthdate"
+                                               value="<?= $data['birthdate'] ?>">
                                     </div>
                                 </div>
                             </div>
                             <div class="form-group mb-3">
                                 <label class="form-label" for="bio">Bio </label>
-                                <textarea name="bio" id="bio" cols="30" rows="5" class="form-control"><?= $data['bio']?></textarea>
+                                <textarea name="bio" id="bio" cols="30" rows="5"
+                                          class="form-control"><?= $data['bio'] ?></textarea>
                             </div>
                             <div class="row">
                                 <div class="col-sm-12 col-md-6">
@@ -53,8 +59,8 @@
                                         <label class="form-label" for="email">Email </label>
                                         <input class="form-control" type="email" name="email" id="email"
                                                autocomplete="off"
-                                               value="<?= $data['email']?>"
-                                               required>
+                                               value="<?= $data['email'] ?>"
+                                        >
                                     </div>
                                 </div>
                                 <div class="col-sm-12 col-md-6">
@@ -62,20 +68,20 @@
                                         <label class="form-label" for="password">Password</label>
                                         <input class="form-control" type="password" name="password" id="password"
                                                autocomplete="off"
-                                               required>
+                                        >
                                     </div>
                                 </div>
                             </div>
                             <div class="form-group mb-3">
                                 <label class="form-label" for="address">Alamat </label>
                                 <textarea name="address" id="address" cols="30" rows="5"
-                                          class="form-control"><?= $data['address']?></textarea>
+                                          class="form-control"><?= $data['address'] ?></textarea>
                             </div>
-                            <br>
+
                             <hr>
                             <div class="row">
                                 <div class="col-md-12 content-right">
-                                    <button class="btn btn-primary form-btn" type="submit">SAVE</button>
+                                    <button class="btn btn-primary form-btn" type="submit" name="submit">SAVE</button>
                                     <a href="home.php" class="btn btn-danger form-btn">CANCEL</a>
                                 </div>
                             </div>
