f4b6cab6ce5378c0620f8333ee491a40a01ab7d8
KgzzzX <aji45802@gmail.com>
2024-10-25 06:24:28 +0700

    update po1 pertemuan 1

---
+++ AjiKartiko/SEMESTER III/PemrogramanObjek1/pertemuan1/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	
+	/**
+	 * Program pertama saya Nama Anda
+	 */
+	
+	public static void main(String[] args){
+	//Menampilkan Kata "Welcome to java programming dilayar"
+		System.out.println("Welcome to java programming");
+	}
+}
