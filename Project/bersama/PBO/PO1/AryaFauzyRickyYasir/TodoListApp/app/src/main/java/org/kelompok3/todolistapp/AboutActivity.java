8dea82252a775d67532e7be5d7ec2563b4859214
Muhamad Yasir Noval <myasirnoval@gmail.com>
2024-12-27 09:49:07 +0700

    aplikasi todolist

---
+++ Project/bersama/PBO/PO1/AryaFauzyRickyYasir/TodoListApp/app/src/main/java/org/kelompok3/todolistapp/AboutActivity.java
@@ -0,0 +1,30 @@
+package org.kelompok3.todolistapp;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        // Add back button in action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("About");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
