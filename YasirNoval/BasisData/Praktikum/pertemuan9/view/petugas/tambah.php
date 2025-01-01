5106fe3a967037c57af02a0af48056a536f599c4
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-01-01 16:34:57 +0700

    add petugas selesai

--- YasirNoval/BasisData/Praktikum/pertemuan9/view/petugas/tambah.php
+++ YasirNoval/BasisData/Praktikum/pertemuan9/view/petugas/tambah.php
@@ -3,19 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Tambah Anggota</title>
+    <title>Tambah Petugas</title>
 </head>
 <body>
-<h2>Tambah Anggota Baru</h2>
+<h2>Tambah Petugas Baru</h2>
 
 <form action="" method="post">
-    <label for="nama_anggota">Nama Anggota:</label><br>
-    <input type="text" name="nama_anggota" id="nama_anggota" required><br><br>
-
-    <label for="alamat">Alamat:</label><br>
-    <textarea name="alamat" id="alamat" cols="30" rows="10" required></textarea>
-    <br><br>
-
+    <label for="nama_petugas">Nama Petugas:</label><br>
+    <input type="text" name="nama_petugas" id="nama_petugas" required><br><br>
+    
     <label for="jenis_kelamin">Jenis Kelamin:</label>
     <select name="jenis_kelamin" id="jenis_kelamin">
         <option value="L">Laki-laki</option>
@@ -25,7 +21,7 @@
     <label for="no_hp">No Telepon:</label>
     <input type="text" name="no_hp" id="no_hp" required><br><br>
 
-    <input type="submit" value="Tambah Anggota" name="submit">
+    <input type="submit" value="Tambah Petugas" name="submit">
 </form>
 <?php
 if (isset($_POST['submit'])) {
@@ -33,18 +29,17 @@ if (isset($_POST['submit'])) {
     include '../../config/koneksi.php';
 
     // mengambil data dari form
-    $nama_anggota = $_POST['nama_anggota'];
-    $alamat = $_POST['alamat'];
+    $nama_petugas = $_POST['nama_petugas'];
     $no_hp = $_POST['no_hp'];
     $jenis_kelamin = $_POST['jenis_kelamin'];
 
-    // query untuk menambahkan data anggota
-    $sql = "INSERT INTO anggota (nama_anggota, alamat, no_hp, jenis_kelamin) VALUES ('$nama_anggota', '$alamat', '$no_hp', '$jenis_kelamin')";
+    // query untuk menambahkan data petugas
+    $sql = "INSERT INTO petugas (nama_petugas, no_hp, jenis_kelamin) VALUES ('$nama_petugas', '$no_hp', '$jenis_kelamin')";
 
     // menjalankan query
     if (mysqli_query($koneksi, $sql)) { ?>
-        <p>Data anggota berhasil ditambahkan!</p>
-        <br><a href="index.php">Kembali ke daftar anggota</a>
+        <p>Data petugas berhasil ditambahkan!</p>
+        <br><a href="index.php">Kembali ke daftar petugas</a>
     <?php } else { ?>
         <p>Terjadi kesalahan: <?php mysqli_error($koneksi); ?></p>
     <?php }
