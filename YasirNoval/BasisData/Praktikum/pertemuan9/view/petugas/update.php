95204400fef9bfee3ccb5c6b04099f10ea871e7e
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-01-01 16:38:24 +0700

    update petugas selesai

--- YasirNoval/BasisData/Praktikum/pertemuan9/view/petugas/update.php
+++ YasirNoval/BasisData/Praktikum/pertemuan9/view/petugas/update.php
@@ -4,17 +4,16 @@ include '../../config/koneksi.php';
 
 // cek apakah data ID yang dikirimkan melalui URL
 if (isset($_GET['id'])) {
-    $anggota_id = $_GET['id'];
+    $petugas_id = $_GET['id'];
 
-    // query untuk mendapatkan data anggota berdasarkan ID
-    $sql = "SELECT * FROM anggota WHERE anggota_id = '$anggota_id'";
+    // query untuk mendapatkan data petugas berdasarkan ID
+    $sql = "SELECT * FROM petugas WHERE petugas_id = '$petugas_id'";
     $result = mysqli_query($koneksi, $sql);
 
     // jika data ditemukan, tampilkan di form
     if (mysqli_num_rows($result) == 1) {
         $row = mysqli_fetch_assoc($result);
-        $nama_anggota = $row['nama_anggota'];
-        $alamat = $row['alamat'];
+        $nama_petugas = $row['nama_petugas'];
         $jenis_kelamin = $row['jenis_kelamin'];
         $no_hp = $row['no_hp'];
     } else {
@@ -28,22 +27,20 @@ if (isset($_GET['id'])) {
 
 // proses update data ketika form disubmit
 if (isset($_POST['submit'])) {
-    $nama_anggota = $_POST['nama_anggota'];
-    $alamat = $_POST['alamat'];
+    $nama_petugas = $_POST['nama_petugas'];
     $jenis_kelamin = $_POST['jenis_kelamin'];
     $no_hp = $_POST['no_hp'];
 
-    // query update data anggota
-    $sql_update = "UPDATE anggota SET 
-                   nama_anggota = '$nama_anggota',
-                   alamat = '$alamat',
+    // query update data petugas
+    $sql_update = "UPDATE petugas SET 
+                   nama_petugas = '$nama_petugas',
                    jenis_kelamin = '$jenis_kelamin$',
                    no_hp = '$no_hp'
-                   WHERE anggota_id = '$anggota_id'";
+                   WHERE petugas_id = '$petugas_id'";
 
     if (mysqli_query($koneksi, $sql_update)) { ?>
-        <p>Data anggota berhasil diperbarui!</p>
-        <a href="index.php">Kembali ke daftar anggota</a>
+        <p>Data petugas berhasil diperbarui!</p>
+        <a href="index.php">Kembali ke daftar petugas</a>
     <?php } else { ?>
         <p>Terjadi kesalahan: <?php mysqli_error($koneksi); ?></p>
     <?php }
@@ -56,16 +53,13 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Update anggota</title>
+    <title>Update petugas</title>
 </head>
 <body>
-<h2>Update Anggota</h2>
+<h2>Update Petugas</h2>
 <form action="" method="post">
-    <label for="nama_anggota">Nama Anggota: </label><br>
-    <input type="text" name="nama_anggota" id="nama_anggota" value="<?= $nama_anggota ?>" required><br><br>
-
-    <label for="alamat">Alamat: </label><br>
-    <textarea name="alamat" id="alamat" cols="30" rows="10" required><?= $alamat ?></textarea><br><br>
+    <label for="nama_petugas">Nama Petugas: </label><br>
+    <input type="text" name="nama_petugas" id="nama_petugas" value="<?= $nama_petugas ?>" required><br><br>
 
     <label for="jenis_kelamin">Jenis Kelamin: </label><br>
     <select name="jenis_kelamin" id="jenis_kelamin">
@@ -76,7 +70,7 @@ if (isset($_POST['submit'])) {
     <label for="no_hp">No Telepon: </label>
     <input type="text" name="no_hp" id="no_hp" value="<?= $no_hp ?>" required><br><br>
 
-    <input type="submit" value="Update Anggota" name="submit">
+    <input type="submit" value="Update Petugas" name="submit">
 </form>
 </body>
 </html>
